fdfd31253f74377016aab3f8c6310e6286489a69
Nathan-Rugo <nathan.githinji@strathmore.edu>
2025-09-26 13:18:39 +0000

    Create init.py

---
+++ init.py
@@ -0,0 +1,5 @@
+def hello(name):
+    print(f"Hello, {name}! Welcome to the Server Administration server hosted by DigitalOcean!!")
+
+userName = input("Please enter your name: \n")
+hello(userName)
